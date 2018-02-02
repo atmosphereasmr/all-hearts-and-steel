67015a0b57e0ef974ee73ccdd138ab362cdfb675
Logan <atmosphereasmr@gmail.com>
2018-02-01 18:11:13 -0800

    okay

--- Server/server.js
+++ Server/server.js
@@ -2,12 +2,11 @@ require('dotenv').config();
 
 const express = require('express')
 , bodyParser = require('body-parser')
+, cors = require('cors')
 , massive = require('massive')
 , controller = require('./controller');
 
 const app = express();
-
-app.use( bodyParser.json() );
 
 app.use(express.static(__dirname + '/../build'))
 
@@ -21,7 +20,11 @@ massive(process.env.CONNECTION_STRING).then(dbInstance => {
 
 app.use(express.static(__dirname + '/build'))
 
+app.use( bodyParser.json() );
+app.use( cors() );
+
+
 app.get('/api/items', controller.getPlanes)
 
-const port = process.env.PORT || 80
+const port = process.env.PORT || 3001
 app.listen( port , () => { console.log(`Server listening on port ${port}`); } );
