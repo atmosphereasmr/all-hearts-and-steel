ccb47bb573e1652974f3e5f5404b7497353796ff
Logan <atmosphereasmr@gmail.com>
2018-02-01 16:28:46 -0800

    fixing

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -5,7 +5,7 @@ import axios from 'axios'
 export default class Header extends Component {
 
   componentDidMount() {
-    axios.get('http://localhost:80/api/items')
+    axios.get('http://www.allheartsandsteel.com/api/items')
     .then(response => {
       console.log(response)
     })
