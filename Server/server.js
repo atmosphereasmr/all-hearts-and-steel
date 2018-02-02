f93844f50386801603425ffb66bfbd2575bdf9a9
Logan <atmosphereasmr@gmail.com>
2018-02-01 18:19:01 -0800

    here we go

--- Server/server.js
+++ Server/server.js
@@ -26,5 +26,11 @@ app.use( cors() );
 
 app.get('/api/items', controller.getPlanes)
 
+let path = require('path');
+app.get('*', (req, res)=>{
+res.sendFile(path.join(__dirname,'../build/index/html');
+
+});
+
 const port = process.env.PORT || 3001
 app.listen( port , () => { console.log(`Server listening on port ${port}`); } );
