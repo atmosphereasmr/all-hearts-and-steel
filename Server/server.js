2cf147fe2c8a7026617adbad8d0ceb91641de20c
Logan <atmosphereasmr@gmail.com>
2018-02-01 19:39:58 -0800

    will i ever get this online God?

--- Server/server.js
+++ Server/server.js
@@ -4,27 +4,27 @@ const express = require('express')
 , bodyParser = require('body-parser')
 , cors = require('cors')
 , massive = require('massive')
-, controller = require('./controller.js');
+, controller = require('./Controllers/controller');
 
 const app = express();
+
+app.use(express.static(__dirname + '/../build'))
 
 massive(process.env.CONNECTION_STRING).then( db => {
  app.set('db', db)
 })
 
-app.use(express.static(__dirname + '/../build'))
+massive(process.env.CONNECTION_STRING).then(dbInstance => {
+ app.set('db', dbInstance);
+});
+
+app.use(express.static(__dirname + '/build'))
 
 app.use( bodyParser.json() );
 app.use( cors() );
 
 
 app.get('/api/items', controller.getPlanes)
-
-let path = require('path');
-app.get('*', (req, res)=>{
-res.sendFile(path.join(__dirname,'../build/index.html');
-
-});
 
 const port = process.env.PORT || 80
 app.listen( port , () => { console.log(`Server listening on port ${port}`); } );
