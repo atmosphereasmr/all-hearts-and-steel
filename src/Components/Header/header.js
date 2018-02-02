46832b1b110ffae597618926166ebb5ed82f50d9
Logan <atmosphereasmr@gmail.com>
2018-02-01 16:49:25 -0800

    redo

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -5,7 +5,7 @@ import axios from 'axios'
 export default class Header extends Component {
 
   componentDidMount() {
-    axios.get('104.131.39.109:80/api/items')
+    axios.get('http://104.131.39.109:80/api/items')
     .then(response => {
       console.log(response)
     })
